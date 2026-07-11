ced47b382bb2e83e086d5ebca972bea643a1097c
Thuto <ThutoSiya@gmail.com>
2026-07-11 21:38:23 +0200

    update transaction validation script to use staging table

--- SQL/02.Validate_Staging_Load/02.Validate_Transactions_Load.sql
+++ SQL/02.Validate_Staging_Load/02.Validate_Transactions_Load.sql
@@ -7,18 +7,18 @@ FROM dwh_Bank.dbo.dwh_transactions
 
 --Preview the data
 SELECT *
-FROM dwh_Bank.dbo.dwh_transactions
+FROM stg_Bank.dbo.stg_transactions
 
 --Check for duplicate transactionID
 SELECT TransactionID,
 COUNT(*) AS Duplicate_Count
-FROM dwh_Bank.dbo.dwh_transactions
+FROM stg_Bank.dbo.stg_transactions
 GROUP BY TransactionID
 HAVING COUNT(*) > 1
 
 --JOIN test
 
 SELECT *
-FROM dwh_Bank.dbo.dwh_transactions AS t
+FROM stg_Bank.dbo.stg_transactions AS t
 JOIN stg_Bank.dbo.stg_customers AS c
 ON t.CustomerID = c.CustomerID
